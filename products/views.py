4a889859060d1b9214140c56c9b5103eb49d256f
Ganesh Pushpala <150124921+pushpalaganesh@users.noreply.github.com>
2025-08-15 16:36:59 +0530

    33522

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.views.generic import TemplateView
 from .models import Item
 from .serializers import ItemSerializer
 
