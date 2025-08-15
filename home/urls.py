f2ea7c62dd1c4ed2b1aabacb3818ec2ce2bad787
Ganesh Pushpala <150124921+pushpalaganesh@users.noreply.github.com>
2025-08-15 13:12:48 +0530

    25455

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import home
 
 urlpatterns = [
-    path('', home, name='home'), # url path of homepage
-    
+    path('', home, name = 'home'), # url path of homepage    
 ]
