4994a9ea2f17fe9986a066c3e4065fcbd71ca9ce
Ganesh Pushpala <150124921+pushpalaganesh@users.noreply.github.com>
2025-10-27 13:54:13 +0530

    53649

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 from django.db import models
@@ -18,3 +19,18 @@ class OrderStatus(models.Model):
 
     def __str__(self):
         return self.name
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    valid_from = models.DateField()
+    valid_until = models.DateField()
+
+    def __str__(self):
+        return f"{self.code} ({self.discount_percentage}% off)"
+
+    def is_valid(self):
+        """Check if the coupon is currently valid."""
+        today = timezone.now().date()
+        return self.is_active and self.valid_from <= today <= self.valid_until
